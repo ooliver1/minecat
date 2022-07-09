69e1bea2d61496ebf58e6a72bd00cd2520cba3b7
ooliver1 <oliverwilkes2006@icloud.com>
2022-07-09 14:06:16 +0100

    chore: use elv2

--- minecat/__main__.py
+++ minecat/__main__.py
@@ -1,3 +1,7 @@
+# Copyright 2022-latest Oliver Wilkes. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with, the Elastic License 2.0
+# https://www.elastic.co/licensing/elastic-license
+
 from __future__ import annotations
 
 from asyncio import set_event_loop_policy
--- minecat/cogs/extras/views.py
+++ minecat/cogs/extras/views.py
@@ -1,3 +1,7 @@
+# Copyright 2022-latest Oliver Wilkes. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with, the Elastic License 2.0
+# https://www.elastic.co/licensing/elastic-license
+
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
