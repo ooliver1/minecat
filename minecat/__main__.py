d09386e80b11fe8bc7f182215650eea888f590a7
ooliver1 <oliverwilkes2006@icloud.com>
2022-07-09 14:22:13 +0100

    feat: add basic starting manager

--- minecat/__main__.py
+++ minecat/__main__.py
@@ -4,20 +4,30 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
 from asyncio import set_event_loop_policy
 from os import getenv
 
+from mineager import run
 from botbase import BotBase
 from dotenv import load_dotenv
 from nextcord import Intents
 from uvloop import EventLoopPolicy
+
+if TYPE_CHECKING:
+    from websockets.server import WebSocketServer
 
 set_event_loop_policy(EventLoopPolicy())
 load_dotenv()
 
 
 class MyBot(BotBase):
-    ...
+    mcws: WebSocketServer
+
+    async def startup(self, *args, **kwargs):
+        self.mcws = await run()
+
+        await super().startup(*args, **kwargs)
 
 
 intents = Intents.none()
