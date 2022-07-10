7a4753ad5e320679802b9546b8128be508739c58
ooliver1 <oliverwilkes2006@icloud.com>
2022-07-10 20:41:11 +0100

    fix: use an __init__ file for version to update

--- minecat/__init__.py
+++ minecat/__init__.py
@@ -2,10 +2,4 @@
 # you may not use this file except in compliance with, the Elastic License 2.0
 # https://www.elastic.co/licensing/elastic-license
 
-from os import getenv
-from minecat import __version__
-
-
-db_url = getenv("DB_URI")
-version = __version__
-logchannel = getenv("LOG_CHANNEL")
+__version__ = "0.1.0"
