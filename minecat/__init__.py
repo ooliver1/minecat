66dcf4ae33d3fe37fade08cc4ac6e696810249a3
Oliver Wilkes <oliverwilkes2006@icloud.com>
2022-07-17 15:40:30 +0100

    chore(master): release 0.2.0 (#3)

--- minecat/__init__.py
+++ minecat/__init__.py
@@ -2,7 +2,7 @@
 # you may not use this file except in compliance with, the Elastic License 2.0
 # https://www.elastic.co/licensing/elastic-license
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 # Copyright 2022-latest Oliver Wilkes. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with, the Elastic License 2.0
 # https://www.elastic.co/licensing/elastic-license
