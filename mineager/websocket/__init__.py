030b51e133cf7b0efe4869315b3f513e1fd61b70
ooliver1 <oliverwilkes2006@icloud.com>
2022-11-09 22:09:12 +0000

    refactor: completely remove everything so keydb can be used

--- mineager/websocket/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2022-latest Oliver Wilkes. Licensed under the Elastic License 2.0;
-# you may not use this file except in compliance with, the Elastic License 2.0
-# https://www.elastic.co/licensing/elastic-license
-
-from .controller import *
-from .logger import *
-from .protocol import *
-from .server import *
-from .storage import *
